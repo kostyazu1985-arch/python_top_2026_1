909889c9a1636eb7b270ac39578169ea10a6d30c
Kostya <kostyazu1985@gmail.com>
2026-03-20 22:32:18 +0300

    add dz

--- Home_work/DZ.py
+++ Home_work/DZ.py
@@ -1,1 +1,14 @@
-reerretttttttttttttttttttttttt
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_name(self):
+        return self.name
+    def get_age(self):
+        return self.age
+
+animal = Animal("Petr", 18)
+print(animal.get_name())
+print(animal.get_age())
+
