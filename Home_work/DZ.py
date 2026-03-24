8b3ad5f189537b6c95801596768e56eec75b48c0
Kostya <kostyazu1985@gmail.com>
2026-03-24 19:55:55 +0300

    add dz

--- Home_work/DZ.py
+++ Home_work/DZ.py
@@ -1,14 +1,90 @@
-class Animal:
-    def __init__(self, name, age):
+# Задача 1
+class Wolf:
+    def howl(self):
+        print("Уууу!")
+
+class Dog:
+    def bark(self):
+        print("Гав!")
+
+class Warewolf(Wolf, Dog):
+    def transform(self):
+        print("Превращение!")
+
+monster = Warewolf()
+
+monster.bark()
+monster.transform()
+monster.howl()
+
+print(Warewolf.__mro__)
+print("-" * 80)
+
+# Задача 2
+class EatMixin:
+    def eat(self):
+        print("Сотрудник ест.")
+
+class SleepMixin:
+    def sleep(self):
+        print("Сотрудник спит.")
+
+class Worker(EatMixin, SleepMixin):
+    def __init__(self, name):
         self.name = name
-        self.age = age
 
-    def get_name(self):
-        return self.name
-    def get_age(self):
-        return self.age
+    def work(self):
+        print(f"{self.name} работает.")
 
-animal = Animal("Petr", 18)
-print(animal.get_name())
-print(animal.get_age())
+employee = Worker("Дмитрий")
+
+employee.sleep()
+employee.work()
+employee.eat()
+
+print("-" * 80)
+
+# Задача 3
+class A:
+    def show(self):
+        print("Класс A.")
+
+class B:
+    def show(self):
+        print("Класс В.")
+
+class C(A, B):
+    def test(self):
+        self.show()
+
+obj = C()
+obj.test()
+# В Python поиск методов идет слева направо по списку родителей.
+# Так как в class C(A, B) класс A стоит первым, его метод "перекрывает" метод класса B.
+
+class C_new(B, A):
+    def test(self):
+        self.show()
+obj_new = C_new()
+obj_new.test()
+
+print("-" * 80)
+
+# Задача 4
+class PrintMixin:
+    def print_document(self, text):
+        print(f"Печать документа: {text}")
+
+class SaveMixin:
+    def save_document(self, text):
+        print(f"Сохранено: {text}")
+
+class Document(SaveMixin, PrintMixin):
+    def create(self, content):
+        self.print_document(content)
+        self.save_document(content)
+
+my_doc = Document()
+my_doc.create("Важный документ")
+
 
