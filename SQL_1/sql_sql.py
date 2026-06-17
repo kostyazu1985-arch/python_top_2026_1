a5dbc8c601148fa7c9fbc9d768f730a8fc728d19
Kostya <kostyazu1985@gmail.com>
2026-06-17 20:04:58 +0300

    add project

---
+++ SQL_1/sql_sql.py
@@ -0,0 +1,111 @@
+"""
+СУБД - Система управления базами данных
+SQL(Structured Query Language) - язык структурированных запросов
+
+столбцы - называются полями(атрибутами), содержат информацию про свойства объектов
+строки - называются записями (кортежи), содержат значения конкретного свойства
+
+SQLite
+
+SQLiteStudio
+DB Browser for SQLite
+
+*.db, *.db3, *.sqlite, *.sqlite3
+
+SELECT [ALL | DISTINCT] (* | столбец1 [,столбец-n])
+FROM таблица1 [, таблица-n]
+WHERE условие
+			AND OR
+			выражение [NOT] BETWEEN начальное_значение AND конечное_значение
+			выражение [NOT] LIKE шаблон_строки
+				% - любое количество символов
+				_ - один любой символ
+			выражение [NOT] GLOB регулярное_выражение
+				* - любое количество знаков
+				? - соответствует одному символу
+				. - соответствует любому одиночному символу
+				[символы] - соответствует любому символу из списка [abc]
+				[начальный-символ-конечный_символ] - соответствует любому одиночному символу из заданного диапазона [0-9]
+				^ - этот символ используется в начале списка и соответствует любому символу, который НЕ входит в список [^0-9]
+			IS [NOT] NULL - позволяет выбирать все строки, столбцы которых имеют значение NULL
+			выражение [NOT] IN (набор значений | выражение)
+ORDER BY col_name | col_number [ASC | DESC]
+INSERT INTO ИМЯ_ТАБЛИЦЫ [столбец1 [, столбец-n]
+VALUE [значение_1, [,значение_2]]
+
+INSERT INTO ИМЯ_ТАБЛИЦЫ [список_столбцов]
+SELECT СПИСОК_СТОЛБЦОВ
+FROM СПИСОК_ТАБЛИЦ
+WHERE УСЛОВИЕ
+"""
+
+# import sqlite3 as sq
+#
+# with sq.connect("profile.db") as con:
+#     cur = con.cursor()
+
+    # СОЗДАНИЕ ТАБЛИЦЫ
+    # cur.execute("""
+    # CREATE TABLE IF NOT EXISTS persons (
+    # id INTEGER PRIMARY KEY AUTOINCREMENT,
+    # name TEXT NOT NULL,
+    # phone BLOB NOT NULL DEFAULT '+79000000000',
+    # age INTEGER NOT NULL CHECK (age > 0 AND age < 120),
+    # email TEXT UNIQUE
+    # )
+    # """)
+
+    # ПЕРЕИМЕНОВАНИЕ ТАБЛИЦЫ
+    # cur.execute("""
+    # ALTER TABLE persons
+    # RENAME TO person_table
+    # """)
+
+    # ДОБАВЛЕНИЕ СТОЛБЦА В ТАБЛИЦУ
+    # cur.execute("""
+    #     ALTER TABLE person_table
+    #     ADD COLUMN address TEXT
+    #     """)
+
+    # УДАЛЕНИЕ СТОЛБЦА ИЗ ТАБЛИЦЫ
+    # cur.execute("""
+    #        ALTER TABLE person_table
+    #        DROP COLUMN address
+    #        """)
+
+    # ПЕРЕИМЕНОВАНИЕ СТОЛБЦА В ТАБЛИЦЕ
+    # cur.execute("""
+    #     ALTER TABLE person_table
+    #     RENAME COLUMN address TO home_address
+    #     """)
+
+    # УДАЛЕНИЕ ТАБЛИЦЫ
+    # cur.execute ("""
+    # DROP TABLE person_table
+    # """)
+
+
+import sqlite3 as sq
+
+with sq.connect("profile.db") as con:
+    cur = con.cursor()
+    # cur.execute("""
+    # CREATE TABLE IF NOT EXISTS persons (
+    # id INTEGER PRIMARY KEY AUTOINCREMENT,
+    # name TEXT NOT NULL,
+    # phone BLOB NOT NULL DEFAULT '+79000000000',
+    # age INTEGER NOT NULL CHECK (age > 0 AND age < 120),
+    # email TEXT UNIQUE
+    # )
+    # """)
+
+    # заполнение таблицы
+#     cur.execute("""
+# INSERT INTO persons
+# VALUES (1, "DMITRY", '+79040648571', 29, 'dimitry88@gmail.com')
+# """)
+
+    cur.execute("""
+INSERT INTO persons (email, name, age)
+VALUES ('anton@gmail.com', 'Антон', 44)
+""")
